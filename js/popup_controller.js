ff30812044443d48a3af9de9d27f2bbf1c1d1284
Fujii Yosuke <fujiyou1121@gmail.com>
2017-04-13 21:56:27 +0900

    ボタンなどのコントローラーを別に

---
+++ js/popup_controller.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', () => {
+  chrome.tabs.getSelected(null, (tab) => {
+    // FIXME: temporary code
+    let url = document.getElementById('page-url');
+    let title = document.getElementById('page-title');
+    url.textContent = tab.url;
+    title.value = tab.title;
+  });
+});
+
+var cancelBtn = document.getElementById('cancel');
+cancelBtn.addEventListener('click', function (e) {
+  window.close();
+});
+
+var okBtn = document.getElementById('ok');
+okBtn.addEventListener('click', function (e) {
+  // クリップボードコピーを呼ぶ
+  // modalを出す。
+  window.close();
+});
