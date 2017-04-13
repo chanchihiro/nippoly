b125585515de64fb7b24acdb3549d82e8a6a9766
Fujii Yosuke <fujiyou1121@gmail.com>
2017-04-13 22:06:58 +0900

    TODO追加

--- js/popup_controller.js
+++ js/popup_controller.js
@@ -1,12 +1,8 @@
-// window.addEventListener('load', () => {
-//   chrome.tabs.getSelected(null, (tab) => {
-//     // FIXME: temporary code
+// TO-DO: Windowがロードされたタイミングで呼び出したい
 //     let url = document.getElementById('page-url');
 //     let title = document.getElementById('page-title');
 //     url.textContent = tab.url;
 //     title.value = tab.title;
-//   });
-// });
 
 var cancelBtn = document.getElementById('cancel');
 cancelBtn.addEventListener('click', function (e) {
