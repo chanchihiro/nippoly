e5ef41303b2d29d7d4d01fa690e64347dd91861b
Fujii Yosuke <fujiyou1121@gmail.com>
2017-04-13 22:04:03 +0900

    コメントアウト

--- js/popup_controller.js
+++ js/popup_controller.js
@@ -1,12 +1,12 @@
-window.addEventListener('load', () => {
-  chrome.tabs.getSelected(null, (tab) => {
-    // FIXME: temporary code
-    let url = document.getElementById('page-url');
-    let title = document.getElementById('page-title');
-    url.textContent = tab.url;
-    title.value = tab.title;
-  });
-});
+// window.addEventListener('load', () => {
+//   chrome.tabs.getSelected(null, (tab) => {
+//     // FIXME: temporary code
+//     let url = document.getElementById('page-url');
+//     let title = document.getElementById('page-title');
+//     url.textContent = tab.url;
+//     title.value = tab.title;
+//   });
+// });
 
 var cancelBtn = document.getElementById('cancel');
 cancelBtn.addEventListener('click', function (e) {
